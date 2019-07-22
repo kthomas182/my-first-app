86039ee6afa349ed7f4210b59dc3f03f82754032
Kyle Thomas <kyle.thomas@essilorusa.com>
2019-07-22 08:58:58 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// @ts-ignore
+import LogRocket from 'logrocket';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-first-app';
+
+  ngOnInit(): void {
+    LogRocket.init('9lhpzm/my-first-app');
+  }
 }
